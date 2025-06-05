269805749b549ea3e29b78c0fb591cd9ac37d587
Bishal Das <bishal049985@gmail.com>
2025-06-05 15:49:21 +0530

    file I/O

---
+++ chapter 9/file1.py
@@ -0,0 +1,14 @@
+'''
+what the file I/O is used is to open a file read or write in it through python.
+'''
+
+# import os
+
+# # Change the working directory to the folder of the script
+# os.chdir(os.path.dirname(__file__))
+
+# Now open the file
+f= open("file.txt")
+data = f.read()
+print(data, end="")
+f.close()
