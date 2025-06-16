b832826a7a9d337116635ad936c9b07c7bcc88fe
Bishal Das <bishal049985@gmail.com>
2025-06-16 14:12:40 +0530

    improved

--- chapter 9/ch9_sol1.py
+++ chapter 9/ch9_sol1.py
@@ -1,7 +1,17 @@
+# with open("forsol1.txt") as f:
+#     read=f.read()
+#     # print(read)
+# if "noice" in read:
+#     print("this file content has the word 'noice'.")
+# else:
+#     print("this file doesn't have the required word.")
+
 with open("forsol1.txt") as f:
     read=f.read()
-    # print(read)
-if "noice" in read:
-    print("this file content has the word 'noice'.")
+    print(read)
+    word=input("enter the word you want to find: ")
+if word in read:
+    print(f"the word '{word}' is there in this file.")
 else:
-    print("this file doesn't have the required word.")
+    print("word doesn't exist in this file.")
+
