a4606cd81da867dcc8dee4a5c18d822423a9ac29
Bishal Das <bishal049985@gmail.com>
2025-07-19 07:25:41 +0530

    new

--- chapter 3/string_functions.py
+++ chapter 3/string_functions.py
@@ -1,14 +1,15 @@
-name=" bishal hi hello  "
+name="bishal hi hello"
 
 #1. to know the length of the string use-
 
 print(len(name))
 
 #2. boolean value if true/false will be shown in the results 
+# the blank spaces gets counted too.
 print(name.endswith("hal"))
-print(name.endswith("hala"))
+print(name.endswith("hello "))
 print(name.startswith("bis"))
-print(name.startswith("bisa"))
+print(name.startswith(" bishal"))
 
 #3. to capitalize the first and only the first character of the string use-
 
