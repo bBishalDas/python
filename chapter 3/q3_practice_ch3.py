0322a2939b90a87539bd80d0c46db4f964048373
Bishal Das <bishal049985@gmail.com>
2025-08-12 22:03:32 +0530

    revision

--- chapter 3/q3_practice_ch3.py
+++ chapter 3/q3_practice_ch3.py
@@ -1,6 +1,6 @@
 # q3. write a program to find double space and q4. replace the double space with single space (strings are immuatabale the string replaced is a whole new string)
 sentance="hi this is  me!!"
-print(sentance, sentance.find("  "), sentance.replace("  ", " "))
+print(f"{(sentance, sentance.find('  '))} \n space filtered text: {sentance.replace('  ', ' ')}")
 
 # q5. use eascape sequance to format this-
 
