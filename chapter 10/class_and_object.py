2548633d8a0a96f7f123dc770ac601559e2a100f
Bishal Das <bishal049985@gmail.com>
2025-09-02 13:44:55 +0530

    class and object

--- chapter 10/class_and_object.py
+++ chapter 10/class_and_object.py
@@ -4,12 +4,12 @@ class car:
     year='2020'
 
 
-# also the instance atrribute has more preferance than the class atrribute
+# also the instance (object) atrribute has more preferance than the class atrribute
 # so if you do 'mycar.color="blue"' blue will be printed instead of red
 mycar= car()
 # mycar.color='blue'
 mycar.name='ruum ruum' # name also called the instance attribute
 print(mycar.brand, mycar.color, mycar.year, mycar.name)
 
-# also the name is n object attribute and the brand, color and year all are class atrribute as 
-# they are directly comes from the class.
+# also the name is an object attribute and the brand, color and year all are class atrribute as 
+# they directly comes from the class.
