f20a6bd687b3c90bf9e9260affaec0080d0e9d3b
Bishal Das <bishal049985@gmail.com>
2025-09-17 17:04:34 +0530

    inheritence

--- chapter 11/inheritance.py
+++ chapter 11/inheritance.py
@@ -1,11 +1,26 @@
 class anime:
-    name= "My dress-up darling"
+    anime_name= "My dress-up darling"
     genre= "rom-com, cosplay"
+    episodes=45
     def manga_adaptation(self):
-        print(f"genre: {self.genre},\nepisodes: {self.episodes}")
+        print(f"anime name: {self.anime_name}\ngenre: {self.genre}\nepisodes: {self.episodes}")
 
-class manga(anime):
-    name="My dress-up darling"
+class manga():
+    manga_name="My dress-up darling"
+    volumes=56
     def anime_adaptation(self):
-        print(f"volumes: {self.volumes}")
+        print(f"manga name:{self.manga_name}\nvolumes: {self.volumes}")
 
+
+class studio(anime,manga):
+    studio_name="MAPPA"
+    def studio_info(self):
+        print(f"name of studio: {self.studio_name}")
+
+a=anime()
+b=studio()
+
+
+b.manga_adaptation()
+b.anime_adaptation()
+b.studio_info()
