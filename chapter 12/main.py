0bbdb6b245a4a70ee046cd0e42343171d95de86d
Bishal Das <bishal049985@gmail.com>
2025-09-29 11:52:04 +0530

    module

--- chapter 12/main.py
+++ chapter 12/main.py
@@ -1,4 +1,9 @@
 from module import myfuc
+
+# another example 
+# from try_finally import prpgram
+
+
 # here the myfuc function is litrally imported from the file named madule.py in this file, also the name module is showen 
 # when this program i runned because of the command print(__name__) in the module file, it displays the file name from where the 
 # function is imported from.
