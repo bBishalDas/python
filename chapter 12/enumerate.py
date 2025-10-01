76a6d21a41a6a15ad2c27a5ffcfd92747ffc8c9a
Bishal Das <bishal049985@gmail.com>
2025-10-01 09:42:35 +0530

    enumerate

---
+++ chapter 12/enumerate.py
@@ -0,0 +1,13 @@
+l=[3,45,67,89,23]
+
+# to assign s.no. to the items in the list 
+index=0
+for item in l:
+    print(f"s.no. {index} for item {item}")
+    index+=1
+
+print("\n")
+# but this can be more simplified by using enumerate
+
+for index, item in enumerate(l):
+    print(f"s.no. {index} for item {item}")
